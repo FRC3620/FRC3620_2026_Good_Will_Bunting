c4ca5d61d38481243f031b68d6ecc082ea6ba5e5
Zayden <13215@stjoebears.com>
2026-01-31 10:58:50 -0500

    Can proofed the spindexer and changed the telemetry and motor names.

--- src/main/java/frc/robot/Subsystems/SpindexerSubsystem.java
+++ src/main/java/frc/robot/Subsystems/SpindexerSubsystem.java
@@ -10,11 +10,10 @@ import static edu.wpi.first.units.Units.DegreesPerSecondPerSecond;
 import static edu.wpi.first.units.Units.Inches;
 import static edu.wpi.first.units.Units.Pounds;
 import static edu.wpi.first.units.Units.RPM;
-import static edu.wpi.first.units.Units.Seconds;
+
+import org.usfirst.frc3620.CANDeviceType;
 
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.sim.TalonFXSimState.MotorType;
-import com.revrobotics.spark.SparkMax;
 
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.system.plant.DCMotor;
@@ -22,6 +21,7 @@ import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.RobotContainer;
 import yams.gearing.GearBox;
 import yams.gearing.MechanismGearing;
 import yams.mechanisms.config.FlyWheelConfig;
@@ -31,90 +31,116 @@ import yams.motorcontrollers.SmartMotorControllerConfig;
 import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
 import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
 import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
-import yams.motorcontrollers.local.SparkWrapper;
 import yams.motorcontrollers.remote.TalonFXWrapper;
 
 public class SpindexerSubsystem extends SubsystemBase {
-  /** Creates a new SpindexerSubsystem. */
+  int motorId = Constants.MOTORID_SPINDEXER;
+  String telemetryPrefix = "Spindexer";
 
-  private SmartMotorControllerConfig smcConfig = new SmartMotorControllerConfig(this)
-      .withControlMode(ControlMode.CLOSED_LOOP)
-      // Feedback Constants (PID Constants)
-      .withClosedLoopController(50, 0, 0, DegreesPerSecond.of(90), DegreesPerSecondPerSecond.of(45))
-      .withSimClosedLoopController(50, 0, 0, DegreesPerSecond.of(90), DegreesPerSecondPerSecond.of(45))
-      // Feedforward Constants
-      .withFeedforward(new SimpleMotorFeedforward(0, 0, 0))
-      .withSimFeedforward(new SimpleMotorFeedforward(0, 0, 0))
-      // Telemetry name and verbosity level
-      .withTelemetry("SpindexerMotor", TelemetryVerbosity.HIGH)
-      // Gearing from the motor rotor to final shaft.
-      // In this example GearBox.fromReductionStages(3,4) is the same as
-      // GearBox.fromStages("3:1","4:1") which corresponds to the gearbox attached to
-      // your motor.
-      // You could also use .withGearing(12) which does the same thing.
-      .withGearing(new MechanismGearing(GearBox.fromReductionStages(3, 4)))
-      // Motor properties to prevent over currenting.
-      .withMotorInverted(false)
-      .withIdleMode(MotorMode.COAST)
-      .withStatorCurrentLimit(Amps.of(40));
+  private TalonFX motor = null;
+  @SuppressWarnings("unused")
+  private SmartMotorController smartMotorController = null;
+  private FlyWheel flywheel = null;
 
-  TalonFX spindexerMotor = new TalonFX(Constants.MOTORID_SPINDEXER);
-  SmartMotorController motor = new TalonFXWrapper(spindexerMotor, DCMotor.getKrakenX60(1), smcConfig);
+  public SpindexerSubsystem() {
+    boolean makeDevices = RobotContainer.canDeviceFinder.isDevicePresent(CANDeviceType.TALON_PHOENIX6, motorId,
+        telemetryPrefix + "Subsystem") || RobotContainer.shouldMakeAllCANDevices();
+    if (makeDevices) {
+      motor = new TalonFX(motorId);
+      SmartMotorControllerConfig smcConfig = new SmartMotorControllerConfig(this)
+          .withControlMode(ControlMode.CLOSED_LOOP)
+          // Feedback Constants (PID Constants)
+          .withClosedLoopController(50, 0, 0, DegreesPerSecond.of(90), DegreesPerSecondPerSecond.of(45))
+          .withSimClosedLoopController(50, 0, 0, DegreesPerSecond.of(90), DegreesPerSecondPerSecond.of(45))
+          // Feedforward Constants
+          .withFeedforward(new SimpleMotorFeedforward(0, 0, 0))
+          .withSimFeedforward(new SimpleMotorFeedforward(0, 0, 0))
+          // Telemetry name and verbosity level
+          .withTelemetry(telemetryPrefix + "Motor", TelemetryVerbosity.HIGH)
+          // Gearing from the motor rotor to final shaft.
+          // In this example GearBox.fromReductionStages(3,4) is the same as
+          // GearBox.fromStages("3:1","4:1") which corresponds to the gearbox attached to
+          // your motor.
+          // You could also use .withGearing(12) which does the same thing.
+          .withGearing(new MechanismGearing(GearBox.fromReductionStages(3, 4)))
+          // Motor properties to prevent over currenting.
+          .withMotorInverted(false)
+          .withIdleMode(MotorMode.COAST)
+          .withStatorCurrentLimit(Amps.of(40));
+      SmartMotorController smartMotorController = new TalonFXWrapper(motor, DCMotor.getKrakenX60(1), smcConfig);
+      FlyWheelConfig flywheelConfig = new FlyWheelConfig(smartMotorController)
+          // Diameter of the flywheel.
+          .withDiameter(Inches.of(4))
+          // Mass of the flywheel.
+          .withMass(Pounds.of(1))
+          // Maximum speed of the flywheel.
+          .withUpperSoftLimit(RPM.of(1000))
+          // Telemetry name and verbosity for the arm.
+          .withTelemetry(telemetryPrefix + "Mech", TelemetryVerbosity.HIGH);
+      flywheel = new FlyWheel(flywheelConfig);
+    }
 
-  private final FlyWheelConfig spindexerConfig = new FlyWheelConfig(motor)
-      // Diameter of the flywheel.
-      .withDiameter(Inches.of(4))
-      // Mass of the flywheel.
-      .withMass(Pounds.of(1))
-      // Maximum speed of the spindexer.
-      .withUpperSoftLimit(RPM.of(1000))
-      // Telemetry name and verbosity for the arm.
-      .withTelemetry("SpindexerMech", TelemetryVerbosity.HIGH);
+  }
 
-  // Spindexer Mechanism
-  private FlyWheel spindexer = new FlyWheel(spindexerConfig);
-
-  /**
-   * Gets the current velocity of the spindexer.
-   *
-   * @return Spindexer velocity.
-   */
-  public AngularVelocity getVelocity() {
-    return spindexer.getSpeed();
+  private Command doNothingCommand() {
+    return run(() -> {
+      // Don't do anything
+    });
   }
 
   /**
-   * Set the spindexer velocity.
+   * Gets the current velocity of the flywheel.
+   *
+   * @return Flywheel velocity.
+   */
+  public AngularVelocity getVelocity() {
+    if (flywheel == null) {
+      return RPM.of(50);
+    } else {
+      return flywheel.getSpeed();
+    }
+  }
+
+  /**
+   * Set the flywheel velocity.
    *
    * @param speed Speed to set.
    * @return {@link edu.wpi.first.wpilibj2.command.RunCommand}
    */
   public Command setVelocityCommand(AngularVelocity speed) {
-    return spindexer.setSpeed(speed);
+    if (flywheel == null) {
+      return doNothingCommand();
+    } else {
+      return flywheel.setSpeed(speed);
+    }
   }
 
   /**
-   * Set the dutycycle of the spindexer.
+   * Set the dutycycle of the flywheel.
    *
    * @param dutyCycle DutyCycle to set.
    * @return {@link edu.wpi.first.wpilibj2.command.RunCommand}
    */
   public Command setDutyCycleCommand(double dutyCycle) {
-    return spindexer.set(dutyCycle);
-  }
-
-  public SpindexerSubsystem() {
-
+    if (flywheel == null) {
+      return doNothingCommand();
+    } else {
+      return flywheel.set(dutyCycle);
+    }
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    spindexer.updateTelemetry();
+    if (flywheel != null) {
+      flywheel.updateTelemetry();
+    }
   }
 
   public void simulationPeriodic() {
     // This method will be called once per scheduler run during simulation
-    spindexer.simIterate();
+    if (flywheel != null) {
+      flywheel.simIterate();
+    }
   }
 }
