156c788bd14c49164a333cd887525360cdd401cf
bennett <14488@stjoebears.com>
2026-01-31 10:27:03 -0500

    testing preshooter in simulation

--- src/main/java/frc/robot/Subsystems/PreshooterSubsystem.java
+++ src/main/java/frc/robot/Subsystems/PreshooterSubsystem.java
@@ -27,61 +27,59 @@ import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
 import yams.motorcontrollers.remote.TalonFXWrapper;
 import org.usfirst.frc3620.CANDeviceFinder;
 
-public class PreshooterSubsystem extends SubsystemBase{
-    private  TalonFX motor=null;
-    private  SmartMotorController motorController;
-    private  FlyWheel flyWheel;
-   
+public class PreshooterSubsystem extends SubsystemBase {
+    private TalonFX motor = null;
+    private SmartMotorController motorController;
+    private FlyWheel flyWheel;
+
     public PreshooterSubsystem() {
-    boolean makeDevices= RobotContainer.canDeviceFinder.isDevicePresent(org.usfirst.frc3620.CANDeviceType.TALON_PHOENIX6, 
-        Constants.MOTORID_PRESHOOTER
-          , "Pre shooter")||RobotContainer.shouldMakeAllCANDevices();
-     if(makeDevices){
+        boolean makeDevices = RobotContainer.canDeviceFinder.isDevicePresent(
+                org.usfirst.frc3620.CANDeviceType.TALON_PHOENIX6,
+                Constants.MOTORID_PRESHOOTER, "Preshooter") || RobotContainer.shouldMakeAllCANDevices();
+        if (makeDevices) {
             motor = new TalonFX(Constants.MOTORID_PRESHOOTER);
-             SmartMotorControllerConfig motorConfig = new SmartMotorControllerConfig(this)
-                .withClosedLoopController(
-                    0.0,  // kP - tune this
-                    0.0,  // kI
-                    0.0,  // kD
-                    RotationsPerSecond.of(0),
-                    RotationsPerSecondPerSecond.of(0)
-                )
-                .withGearing(new MechanismGearing(GearBox.fromReductionStages(1, 1))) // Direct drive
-                .withIdleMode(MotorMode.BRAKE)
-                .withTelemetry("Pre shooter", TelemetryVerbosity.HIGH)
-                .withStatorCurrentLimit(Amps.of(40))
-                .withSupplyCurrentLimit(Amps.of(40))
-                .withControlMode(ControlMode.CLOSED_LOOP);
-            
-                motorController = new TalonFXWrapper(motor, DCMotor.getKrakenX60(1), motorConfig);
-                FlyWheelConfig rollerConfig = new FlyWheelConfig(motorController)
+            SmartMotorControllerConfig motorConfig = new SmartMotorControllerConfig(this)
+                    .withClosedLoopController(
+                            0.0, // kP - tune this
+                            0.0, // kI
+                            0.0, // kD
+                            RotationsPerSecond.of(0),
+                            RotationsPerSecondPerSecond.of(0))
+                    .withGearing(new MechanismGearing(GearBox.fromReductionStages(1, 1))) // Direct drive
+                    .withIdleMode(MotorMode.BRAKE)
+                    .withTelemetry("PreshooterMotor", TelemetryVerbosity.HIGH)
+                    .withStatorCurrentLimit(Amps.of(40))
+                    .withSupplyCurrentLimit(Amps.of(40))
+                    .withControlMode(ControlMode.CLOSED_LOOP);
+
+            motorController = new TalonFXWrapper(motor, DCMotor.getKrakenX60(2), motorConfig);
+            FlyWheelConfig rollerConfig = new FlyWheelConfig(motorController)
                     .withDiameter(Inch.of(0))
                     .withMass(Pound.of(0))
                     .withUpperSoftLimit(RPM.of(0))
-                    .withTelemetry("Pre Shooter", TelemetryVerbosity.HIGH);
-                
-                // Create the FlyWheel
-                flyWheel = new FlyWheel(rollerConfig);
+                    .withTelemetry("PreShooter", TelemetryVerbosity.HIGH);
+
+            // Create the FlyWheel
+            flyWheel = new FlyWheel(rollerConfig);
         }
     }
- public Command rollersOn() {
+
+    public Command rollersOn() {
         // Only use YAMS control, not manual rollers.set()
         return flyWheel.setSpeed(RPM.of(1500)).withName("shooter On");
     }
-    
+
     public Command rollersOff() {
         return flyWheel.setSpeed(RPM.of(0)).withName("shooter Off");
     }
-    
+
     public Command rollersBackwards() {
         return flyWheel.setSpeed(RPM.of(-1500)).withName("shooter Backwards");
     }
-    
+
     @Override
     public void simulationPeriodic() {
         // Only simulate, don't manually run the roller
         flyWheel.simIterate();
     }
-}
-
-
+ }
