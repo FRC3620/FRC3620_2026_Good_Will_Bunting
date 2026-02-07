89aa5c293b23146ed244884aabe2958c9bcb3832
Zayden <13215@stjoebears.com>
2026-02-07 15:23:20 -0500

    Made Blinky Lights flash and made the color change when Teleop is disabled and enabled. Fixes #84

--- src/main/java/frc/robot/Subsystems/BlinkyLightsSubsystem.java
+++ src/main/java/frc/robot/Subsystems/BlinkyLightsSubsystem.java
@@ -4,27 +4,47 @@
 
 package frc.robot.Subsystems;
 
+import static edu.wpi.first.units.Units.Centimeters;
 import static edu.wpi.first.units.Units.Meters;
 import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.Percent;
+import static edu.wpi.first.units.Units.Second;
+import static edu.wpi.first.units.Units.Seconds;
+
+import java.util.Map;
+
+import org.usfirst.frc3620.RobotMode;
+
+import com.ctre.phoenix6.controls.RainbowAnimation;
 
 import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.AddressableLEDBufferView;
 import edu.wpi.first.wpilibj.LEDPattern;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Robot;
 
 public class BlinkyLightsSubsystem extends SubsystemBase {
+  int length = 38;
+
   AddressableLED m_led = new AddressableLED(9);
 
-  AddressableLEDBuffer m_buffer = new AddressableLEDBuffer(40);
-   LEDPattern pattern = LEDPattern.rainbow(255, 200);
-   private static final Distance kLedSpacing = Meters.of(1 / 120.0);
-  private final LEDPattern patternrainbowscroll = pattern.scrollAtAbsoluteSpeed(MetersPerSecond.of(1), kLedSpacing);
+  AddressableLEDBuffer m_buffer = new AddressableLEDBuffer(length);
+
+  Distance ledSpacing = Meters.of(1 / 120.0);
+  LEDPattern base = LEDPattern.solid(Color.kRed);
+  LEDPattern driverDisabled = LEDPattern.solid(Color.kOrange);
+  LEDPattern driverTeleop = LEDPattern.solid(Color.kGreen);
+  LEDPattern critical = base.blink(Seconds.of(0.5));
+
+  AddressableLEDBufferView m_health = m_buffer.createView(0, length / 2);
+  AddressableLEDBufferView m_driver = m_buffer.createView(length / 2 + 1, length - 1).reversed();
 
   /** Creates a new BlinkyLightsSubsystem. */
   public BlinkyLightsSubsystem() {
-    m_led.setLength(40);
+    m_led.setLength(length);
     m_led.start();
   }
 
@@ -32,9 +52,17 @@ public class BlinkyLightsSubsystem extends SubsystemBase {
   public void periodic() {
     // This method will be called once per scheduler run
     // Create an LED pattern that sets the entire strip to solid red
-   
+
     // Apply the LED pattern to the data buffer
-    patternrainbowscroll.applyTo(m_buffer);
+    critical.applyTo(m_health);
+
+    RobotMode robotMode = Robot.getCurrentRobotMode();
+    if (robotMode == RobotMode.TELEOP) {
+      driverTeleop.applyTo(m_driver);
+    } 
+    if (robotMode == RobotMode.DISABLED) {
+      driverDisabled.applyTo(m_driver);
+    }
 
     // Write the data to the LED strip
     m_led.setData(m_buffer);
