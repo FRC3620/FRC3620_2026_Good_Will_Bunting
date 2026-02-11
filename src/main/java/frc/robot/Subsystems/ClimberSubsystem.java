eb34d419249f82b80d4389fbbfa7d4505a933a59
Doug Wegscheid <dwegscheid@sbcglobal.net>
2026-02-10 22:13:22 -0500

    More cleanup

--- src/main/java/frc/robot/Subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/Subsystems/ClimberSubsystem.java
@@ -5,7 +5,6 @@
 package frc.robot.Subsystems;
 
 import static edu.wpi.first.units.Units.Amps;
-import static edu.wpi.first.units.Units.Inch;
 import static edu.wpi.first.units.Units.Inches;
 import static edu.wpi.first.units.Units.Meters;
 import static edu.wpi.first.units.Units.MetersPerSecond;
@@ -16,8 +15,6 @@ import static edu.wpi.first.units.Units.Seconds;
 import org.usfirst.frc3620.CANDeviceType;
 
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.sim.TalonFXSimState.MotorType;
-import com.revrobotics.spark.SparkMax;
 
 import edu.wpi.first.math.controller.ElevatorFeedforward;
 import edu.wpi.first.math.geometry.Translation3d;
@@ -31,7 +28,6 @@ import yams.gearing.GearBox;
 import yams.gearing.MechanismGearing;
 import yams.mechanisms.config.ElevatorConfig;
 import yams.mechanisms.config.MechanismPositionConfig;
-import yams.mechanisms.config.MechanismPositionConfig.Plane;
 import yams.mechanisms.positional.Elevator;
 import yams.motorcontrollers.SmartMotorController;
 import yams.motorcontrollers.SmartMotorControllerConfig;
@@ -41,63 +37,63 @@ import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
 import yams.motorcontrollers.remote.TalonFXWrapper;
 
 public class ClimberSubsystem extends SubsystemBase {
-  
-  private SmartMotorControllerConfig smcConfig = null;
-  private TalonFX motor=null;
-  SmartMotorController motorController =null;
-  private ElevatorConfig elevconfig =null;
+  int motorId = Constants.MOTORID_CLIMBER;
+  String telemetryPrefix = "Climber";
+
+  private TalonFX motor = null;
+  SmartMotorController motorController = null;
   private Elevator elevator = null;
-//
 
-  private boolean makeDevice= RobotContainer.canDeviceFinder.isDevicePresent(CANDeviceType.CANCODER_PHOENIX6,
-   Constants.MOTORID_CLIMBER)|| RobotContainer.shouldMakeAllCANDevices();
+  public ClimberSubsystem() {
 
-   public ClimberSubsystem() {
-    if(makeDevice){
-      motor= new TalonFX(Constants.MOTORID_CLIMBER);
-      smcConfig =new SmartMotorControllerConfig(this)
-      .withControlMode(ControlMode.CLOSED_LOOP)
-      // Mechanism Circumference is the distance traveled by each mechanism rotation
-      // converting rotations to meters.
-      .withMechanismCircumference(Meters.of(Inches.of(0.25).in(Meters) * 22))
-      // Feedback Constants (PID Constants)
-      .withClosedLoopController(4, 0, 0, MetersPerSecond.of(0.5), MetersPerSecondPerSecond.of(0.5))
-      .withSimClosedLoopController(4, 0, 0, MetersPerSecond.of(0.5), MetersPerSecondPerSecond.of(0.5))
-      // Feedforward Constants
-      .withFeedforward(new ElevatorFeedforward(0, 0, 0))
-      .withSimFeedforward(new ElevatorFeedforward(0, 0, 0))
-      // Telemetry name and verbosity level
-      .withTelemetry("ElevatorMotor", TelemetryVerbosity.HIGH)
-      // Gearing from the motor rotor to final shaft.
-      // In this example gearbox(3,4) is the same as gearbox("3:1","4:1") which
-      // corresponds to the gearbox attached to your motor.
-      .withGearing(new MechanismGearing(GearBox.fromReductionStages(3, 4)))
-      // Motor properties to prevent over currenting.
-      .withMotorInverted(false)
-      .withIdleMode(MotorMode.BRAKE)
-      .withStatorCurrentLimit(Amps.of(40))
-      .withClosedLoopRampRate(Seconds.of(0.25))
-      .withOpenLoopRampRate(Seconds.of(0.25));
+    boolean makeDevice = RobotContainer.canDeviceFinder.isDevicePresent(CANDeviceType.CANCODER_PHOENIX6,
+        motorId) || RobotContainer.shouldMakeAllCANDevices();
+    if (makeDevice) {
+      motor = new TalonFX(motorId);
+      motor.getFaultField();
+      motor.getFault_DeviceTemp();
+      SmartMotorControllerConfig smcConfig = new SmartMotorControllerConfig(this)
+          .withControlMode(ControlMode.CLOSED_LOOP)
+          // Mechanism Circumference is the distance traveled by each mechanism rotation
+          // converting rotations to meters.
+          .withMechanismCircumference(Meters.of(Inches.of(0.25).in(Meters) * 22))
+          // Feedback Constants (PID Constants)
+          .withClosedLoopController(4, 0, 0, MetersPerSecond.of(0.5), MetersPerSecondPerSecond.of(0.5))
+          .withSimClosedLoopController(4, 0, 0, MetersPerSecond.of(0.5), MetersPerSecondPerSecond.of(0.5))
+          // Feedforward Constants
+          .withFeedforward(new ElevatorFeedforward(0, 0, 0))
+          .withSimFeedforward(new ElevatorFeedforward(0, 0, 0))
+          // Telemetry name and verbosity level
+          .withTelemetry("ElevatorMotor", TelemetryVerbosity.HIGH)
+          // Gearing from the motor rotor to final shaft.
+          // In this example gearbox(3,4) is the same as gearbox("3:1","4:1") which
+          // corresponds to the gearbox attached to your motor.
+          .withGearing(new MechanismGearing(GearBox.fromReductionStages(3, 4)))
+          // Motor properties to prevent over currenting.
+          .withMotorInverted(false)
+          .withIdleMode(MotorMode.BRAKE)
+          .withStatorCurrentLimit(Amps.of(40))
+          .withClosedLoopRampRate(Seconds.of(0.25))
+          .withOpenLoopRampRate(Seconds.of(0.25));
 
-      motorController= new TalonFXWrapper(motor,
-      DCMotor.getKrakenX60(1),
-      smcConfig);
+      motorController = new TalonFXWrapper(motor,
+          DCMotor.getKrakenX60(1),
+          smcConfig);
 
-      elevconfig= new ElevatorConfig(motorController)
-      .withStartingHeight(Meters.of(0.5))
-      .withHardLimits(Meters.of(0), Meters.of(3))
-      .withTelemetry("Elevator", TelemetryVerbosity.HIGH)
-      .withMechanismPositionConfig(
-          new MechanismPositionConfig()
-              .withMaxRobotHeight(Inches.of(30))
-              .withMaxRobotLength(Inches.of(34))
-              .withRelativePosition(new Translation3d(Inches.of(0), Inches.of(0), Inches.of(0))))
-      .withMass(Pounds.of(16));
+      ElevatorConfig config = new ElevatorConfig(motorController)
+          .withStartingHeight(Meters.of(0.5))
+          .withHardLimits(Meters.of(0), Meters.of(3))
+          .withTelemetry("Elevator", TelemetryVerbosity.HIGH)
+          .withMechanismPositionConfig(
+              new MechanismPositionConfig()
+                  .withMaxRobotHeight(Inches.of(30))
+                  .withMaxRobotLength(Inches.of(34))
+                  .withRelativePosition(new Translation3d(Inches.of(0), Inches.of(0), Inches.of(0))))
+          .withMass(Pounds.of(16));
 
-      elevator=new Elevator(elevconfig); 
+      elevator = new Elevator(config);
     }
-   }
-   
+  }
 
   /**
    * Set the height of the elevator.
@@ -105,10 +101,14 @@ public class ClimberSubsystem extends SubsystemBase {
    * @param angle Distance to go to.
    */
   public Command setHeight(Distance height) {
+    Command rv;
     if (elevator == null) {
-      return null;
+      rv = run(() -> {
+      });
+    } else {
+      rv = elevator.setHeight(height);
     }
-    return elevator.setHeight(height);
+    return rv.withName("SetHeight");
   }
 
   /**
@@ -117,10 +117,14 @@ public class ClimberSubsystem extends SubsystemBase {
    * @param dutycycle [-1, 1] speed to set the elevator too.
    */
   public Command set(double dutycycle) {
+    Command rv;
     if (elevator == null) {
-      return null;
+      rv = run(() -> {
+      });
+    } else {
+      rv = elevator.set(dutycycle);
     }
-    return elevator.set(dutycycle);
+    return rv.withName("Set");
   }
 
   /** Creates a new ExampleSubsystem. */
