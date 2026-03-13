482d0400e58582d673b1431ad9b6c2c25cffff87
Atharv <15321@stjoebears.com>
2026-03-13 15:14:44 -0400

    passing enum is correct now

--- src/main/java/frc/robot/Helpers/ShotCalculator.java
+++ src/main/java/frc/robot/Helpers/ShotCalculator.java
@@ -31,10 +31,21 @@ public class ShotCalculator {
 
     public enum FieldTargets {
         //EXACT CENTER OF HUB
+
         BLUE_HUB(new Translation3d(Inches.of(182.11), Inches.of(158.84), Inches.of(72))),
         //TBD
         OP_PASS(new Translation3d(Inches.of(90), Inches.of(60), Inches.of(0))),
-        DEPOT_PASS(new Translation3d(Inches.of(90), Inches.of(180), Inches.of(0)));      
+        DEPOT_PASS(new Translation3d(Inches.of(90), Inches.of(180), Inches.of(0)));     
+        
+        private final Translation3d targetPosition;
+
+        FieldTargets(Translation3d targetPosition) {
+            this.targetPosition = targetPosition;
+        }
+
+        public Translation3d getTargetPosition() {
+            return targetPosition;
+        }
     }
 
     private static final double ratioOverMinVelocity = 1.03; // placeholder value
