85677fb34238e68d0e4bf71161c0518509d293da
Albert <12158@stjoebears.com>
2026-03-20 14:37:24 -0400

    what we're working with at berrien springs

--- src/main/java/frc/robot/fsm/StateMachine.java
+++ src/main/java/frc/robot/fsm/StateMachine.java
@@ -1,10 +1,7 @@
 package frc.robot.fsm;
-
 import java.util.Optional;
-
 import org.tinylog.TaggedLogger;
 import org.usfirst.frc3620.logger.LoggingMaster;
-
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Robot;
@@ -12,34 +9,25 @@ import frc.robot.RobotContainer;
 import frc.robot.fsm.states.IState;
 
 public class StateMachine {
-
     public final static TaggedLogger logger = LoggingMaster.getLogger(StateMachine.class);
-
     private boolean useStateMachine = true;
-
+    private boolean hasEnteredInitialState = false;
     private IState currentState;
 
     public StateMachine(IState initialState) {
-        if(!useStateMachine) {
-            if (DriverStation.isAutonomous()) {
-                this.currentState = initialState;
-            }else{
-                this.currentState = initialState;
-                this.currentState.onEnter();
-            }
-        } 
-        else 
-        {
-            this.currentState = initialState;
-        }
+        this.currentState = initialState;
     }
 
     public void update() {
         if (!useStateMachine) {
             SmartDashboard.putString("FSM Current State", "DISABLED");
-        } else if(DriverStation.isAutonomous()) {
+        } else if (DriverStation.isAutonomous()) {
             SmartDashboard.putString("FSM Current State", "Running Auto");
         } else {
+            if (!hasEnteredInitialState) {
+                currentState.onEnter();
+                hasEnteredInitialState = true;
+            }
             currentState.execute();
             SmartDashboard.putString("FSM Current State", currentState.getClass().getSimpleName());
             Optional<IState> nextState = currentState.nextState();
@@ -49,7 +37,7 @@ public class StateMachine {
                 currentState.onEnter();
             }
         }
-        if(currentState != null) {
+        if (currentState != null) {
             SmartDashboard.putBoolean("frc3620/StateMachine/useStateMachine?", useStateMachine);
             SmartDashboard.putBoolean("frc3620/StateMachine/isActivePeriod", RobotContainer.fmsTriggers.isActivePeriod.getAsBoolean());
         }
