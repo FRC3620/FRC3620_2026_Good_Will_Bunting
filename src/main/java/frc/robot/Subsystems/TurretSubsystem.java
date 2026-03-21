9a4f68c496a8fe4fc2332dffdc41deaa16e1f3c9
Grace <10039@stjoebears.com>
2026-03-20 21:29:01 -0400

    Berrien Springs Friday Updates

--- src/main/java/frc/robot/Subsystems/TurretSubsystem.java
+++ src/main/java/frc/robot/Subsystems/TurretSubsystem.java
@@ -76,8 +76,8 @@ public class TurretSubsystem extends SubsystemBase {
   private SmartMotorController smartMotorController = null;
   private Pivot pivot = null;
 
-  private static final Angle absAEncoderOffset = Rotations.of(-0.50048828125);
-  private static final Angle absBEncoderOffset = Rotations.of(-0.77734375);
+  private static final Angle absAEncoderOffset = Rotations.of(-0.4560546875);
+  private static final Angle absBEncoderOffset = Rotations.of(-0.731689453125);
 
   private SlewRateLimiter turretLimiter = new SlewRateLimiter(180.0);
   private Angle filteredTargetAngle = Degrees.of(0);
