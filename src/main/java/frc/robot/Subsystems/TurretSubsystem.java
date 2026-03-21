b073df54fed5efae2aa02551739c16e9d54e8538
Grace <10039@stjoebears.com>
2026-03-21 09:27:09 -0400

    turret pluh

--- src/main/java/frc/robot/Subsystems/TurretSubsystem.java
+++ src/main/java/frc/robot/Subsystems/TurretSubsystem.java
@@ -76,8 +76,8 @@ public class TurretSubsystem extends SubsystemBase {
   private SmartMotorController smartMotorController = null;
   private Pivot pivot = null;
 
-  private static final Angle absAEncoderOffset = Rotations.of(-0.4560546875);
-  private static final Angle absBEncoderOffset = Rotations.of(-0.731689453125);
+  private static final Angle absAEncoderOffset = Rotations.of(-0.48486328125);
+  private static final Angle absBEncoderOffset = Rotations.of(-0.76318359375);
 
   private SlewRateLimiter turretLimiter = new SlewRateLimiter(180.0);
   private Angle filteredTargetAngle = Degrees.of(0);
