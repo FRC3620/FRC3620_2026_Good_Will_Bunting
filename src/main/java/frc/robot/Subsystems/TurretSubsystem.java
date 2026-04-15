5f8fc3fc13a8677ce4e1cfed94233aaf3e24f9f5
Albert <12158@stjoebears.com>
2026-04-15 14:20:03 -0400

    turret ACT

--- src/main/java/frc/robot/Subsystems/TurretSubsystem.java
+++ src/main/java/frc/robot/Subsystems/TurretSubsystem.java
@@ -79,8 +79,8 @@ public class TurretSubsystem extends SubsystemBase {
   private SmartMotorController smartMotorController = null;
   private Pivot pivot = null;
 
-  private static final Angle absAEncoderOffset = Rotations.of(-0.667724609375);
-  private static final Angle absBEncoderOffset = Rotations.of(-0.421142578125);
+  private static final Angle absAEncoderOffset = Rotations.of(-0.093017578125);
+  private static final Angle absBEncoderOffset = Rotations.of(-0.76708984375);
 
   private Angle filteredTargetAngle = Degrees.of(0);
   private double turretTargetingOffset = 0;
